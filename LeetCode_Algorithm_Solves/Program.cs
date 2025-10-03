ab4455754625b0a99ec91d0798f1b6c8c6ec44c9
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-03 23:09:23 +0600

    13. Roman to Integer

--- LeetCode_Algorithm_Solves/Program.cs
+++ LeetCode_Algorithm_Solves/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace LeetCode_Algorithm_Solves
 {
@@ -54,7 +55,30 @@ namespace LeetCode_Algorithm_Solves
 
         #region 13. Roman to Integer
 
-        
+        public int RomanToInt(string s) {
+            Dictionary<char, int> romanValues = new Dictionary<char, int> 
+            {
+                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+                {'C', 100}, {'D', 500}, {'M', 1000}
+            };
+
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++) 
+            {
+                if (i + 1 < s.Length && romanValues[s[i]] < romanValues[s[i + 1]]) 
+                {
+                    result -= romanValues[s[i]];
+                } 
+                else 
+                {
+                    result += romanValues[s[i]];
+                }
+            }
+
+            return result;
+        }
+
 
         #endregion
         #region 9. Palindrome Number
