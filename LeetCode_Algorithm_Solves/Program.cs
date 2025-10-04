60816962ee94f4c6d229e0490aab5ac1ba613722
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-04 19:56:54 +0600

    4. Median of Two Sorted Arrays

--- LeetCode_Algorithm_Solves/Program.cs
+++ LeetCode_Algorithm_Solves/Program.cs
@@ -45,40 +45,76 @@ namespace LeetCode_Algorithm_Solves
             // Console.WriteLine(IsPalindrome(121));
 
             #endregion
-            #region 13. Roman to Integer
-
-        
-
+            #region    4. Median of Two Sorted Arrays
+            double median = FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 });
+            Console.WriteLine($"Median is: {median}");
             #endregion
             
         }
 
-        #region 13. Roman to Integer
+        #region    4. Median of Two Sorted Arrays
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+            // Ensure nums1 is the smaller array
+            if (nums1.Length > nums2.Length) {
+                return FindMedianSortedArrays(nums2, nums1);
+            }
 
-        public int RomanToInt(string s) {
-            Dictionary<char, int> romanValues = new Dictionary<char, int> 
-            {
-                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-                {'C', 100}, {'D', 500}, {'M', 1000}
-            };
+            int x = nums1.Length;
+            int y = nums2.Length;
+            int low = 0, high = x;
 
-            int result = 0;
+            while (low <= high) {
+                int partitionX = (low + high) / 2;
+                int partitionY = (x + y + 1) / 2 - partitionX;
 
-            for (int i = 0; i < s.Length; i++) 
-            {
-                if (i + 1 < s.Length && romanValues[s[i]] < romanValues[s[i + 1]]) 
-                {
-                    result -= romanValues[s[i]];
-                } 
-                else 
-                {
-                    result += romanValues[s[i]];
+                int maxLeftX = (partitionX == 0) ? int.MinValue : nums1[partitionX - 1];
+                int minRightX = (partitionX == x) ? int.MaxValue : nums1[partitionX];
+
+                int maxLeftY = (partitionY == 0) ? int.MinValue : nums2[partitionY - 1];
+                int minRightY = (partitionY == y) ? int.MaxValue : nums2[partitionY];
+
+                if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                    if ((x + y) % 2 == 0) {
+                        return (Math.Max(maxLeftX, maxLeftY) + Math.Min(minRightX, minRightY)) / 2.0;
+                    } else {
+                        return Math.Max(maxLeftX, maxLeftY);
+                    }
+                } else if (maxLeftX > minRightY) {
+                    high = partitionX - 1;
+                } else {
+                    low = partitionX + 1;
                 }
             }
 
-            return result;
+            throw new ArgumentException("Input arrays are not sorted properly.");
         }
 
+        #endregion
+        #region 13. Roman to Integer
+
+        // public int RomanToInt(string s) {
+        //     Dictionary<char, int> romanValues = new Dictionary<char, int> 
+        //     {
+        //         {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        //         {'C', 100}, {'D', 500}, {'M', 1000}
+        //     };
+        //
+        //     int result = 0;
+        //
+        //     for (int i = 0; i < s.Length; i++) 
+        //     {
+        //         if (i + 1 < s.Length && romanValues[s[i]] < romanValues[s[i + 1]]) 
+        //         {
+        //             result -= romanValues[s[i]];
+        //         } 
+        //         else 
+        //         {
+        //             result += romanValues[s[i]];
+        //         }
+        //     }
+        //
+        //     return result;
+        // }
 
         #endregion
         #region 9. Palindrome Number
